8eb97a25c391206f9af8f98bdd76621f5b63d119
hashashin301236 <dilstudio29@gmail.com>
2017-01-27 13:38:32 +0700

    Add js files via upload.

---
+++ js/sidenav.js
@@ -0,0 +1,19 @@
+function openNav() {
+document.getElementById("mySidenav").style.width = "100%";
+document.getElementById("main").style.marginRight = "320px";
+}
+
+function closeNav() {
+document.getElementById("mySidenav").style.width = "0";
+document.getElementById("main").style.marginRight = "0";
+}
+            
+function openNavLeft() {
+document.getElementById("mySidenavLeft").style.width = "300px";
+document.getElementById("main").style.marginLeft = "300px";
+}
+
+function closeNavLeft() {
+document.getElementById("mySidenavLeft").style.width = "0";
+document.getElementById("main").style.marginLeft = "0";
+}
